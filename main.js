af6f024015cb2eae4e0c31e03aef0af48a985c9d
juliandeligt <82270985+juliandeligt@users.noreply.github.com>
2024-09-02 19:40:21 +0200

    Update main.js

--- main.js
+++ main.js
@@ -100,6 +100,23 @@ document.addEventListener('DOMContentLoaded', function() {
     } else {
         scrollProject(currentIndex);
     }
+
+    // Form submission
+    function submitForm(event) {
+        event.preventDefault();
+
+        var name = document.getElementById('name').value;
+        var subject = document.getElementById('subject').value;
+        var message = document.getElementById('message').value;
+
+        var mailtoLink = 'mailto:juliandeligt@hotmail.nl' +
+            '?subject=' + encodeURIComponent(subject) +
+            '&body=' + encodeURIComponent('Name: ' + name + '\n\nMessage:\n' + message);
+
+        window.location.href = mailtoLink;
+    }
+
+    document.getElementById('contact-form').addEventListener('submit', submitForm);
 });
 
 // Fetch and display project data
@@ -111,23 +128,26 @@ document.addEventListener('DOMContentLoaded', () => {
                 throw new Error(`HTTP error! status: ${response.status}`);
             }
             const data = await response.json();
-    
+
+            // Get all elements with IDs starting with 'projectTemplate'
             const containers = document.querySelectorAll('[id^="projectTemplate"]');
-    
+            
             containers.forEach(container => {
                 const containerId = container.id;
                 const projectName = containerId
-                    .replace('projectTemplate', '')
-                    .replace(/([A-Z])/g, ' $1')
-                    .trim();
-    
+                    .replace('projectTemplate', '') // Remove the prefix
+                    .replace(/([A-Z])/g, ' $1') // Add spaces before capital letters
+                    .trim(); // Remove extra spaces
+
+                // Find the project based on the name
                 const project = data.portfolio_projects.find(p => p.project_name === projectName);
-    
+
                 if (project) {
+                    // Conditionally add project services if not empty
                     const services = [project.project_service_1, project.project_service_2, project.project_service_3]
-                        .filter(service => service)
+                        .filter(service => service) // Filter out empty services
                         .map(service => `<p>${service}</p>`).join('');
-    
+
                     const projectTemplate = `
                         <div class="project-section-1">
                             <div class="row-1">
@@ -139,6 +159,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 </div>
                             </div>
                         </div>
+
                         <div class="project-section-2">
                             <div class="row-1">
                                 <div class="col-1 col">
@@ -171,9 +192,10 @@ document.addEventListener('DOMContentLoaded', () => {
                                 <div class="row-1"></div>
                             </div>
                         </div>
-                        <div class="project-banner-2">
-                            <div class="row-1"></div>
-                        </div>
+                            <div class="project-banner-2">
+                                <div class="row-1"></div>
+                            </div>
+
                         <div class="project-section-3">
                             <div class="row-1">
                                 <h2>Het proces</h2>
@@ -186,6 +208,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 <div class="col col-3"></div>
                             </div>
                         </div>
+
                         <div class="project-section-4">
                             <div class="row-2">
                                 <div class="col-1">
@@ -196,6 +219,7 @@ document.addEventListener('DOMContentLoaded', () => {
                                 </div>
                             </div>
                         </div>
+
                         <div class="home-section-5">
                             <div class="row-1">
                                 <div class="col-1">
@@ -207,52 +231,51 @@ document.addEventListener('DOMContentLoaded', () => {
                                 </div>
                             </div>
                             <div class="row-2">
-                                <form action="/succes.html" method="post" data-netlify="true">
+                                <form action="/succes" method="post" data-netlify="true">
                                     <label for="name">NAAM:</label><br />
                                     <input type="text" id="name" name="name" /><br /><br />
-    
+
                                     <label for="email">E-MAILADRES:</label><br />
                                     <input type="text" id="email" name="email" /><br /><br />
-    
+
                                     <label for="subject">ONDERWERP:</label><br />
                                     <input type="text" id="subject" name="subject" /><br /><br />
-    
+
                                     <label for="message">BERICHT:</label><br />
                                     <textarea id="message" name="message"></textarea><br /><br />
                                     <button type="submit">VERSTUREN</button>
                                 </form>
-                            </div> 
-                        </div>
-                        <footer>
-                            <div class="row-1">
-                                <div class="col-1"><p>© Julian de Ligt 2024</p></div>
-                                <div class="col-2">
-                                    <a
-                                    href="https://www.linkedin.com/in/julian-de-ligt-1586b9201?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
-                                    target="_blank"
-                                    >
-                                    <img
-                                        src="icons/logo-linkedin.svg"
-                                        alt="LinkedIn logo"
-                                        width="40"
-                                        height="40"
-                                    />
-                                    </a>
-                                    <a
-                                    href="https://www.instagram.com/juliandeligt_?igsh=bmNwbWdleWJnNWFp&utm_source=qr"
-                                    target="_blank"
-                                    >
-                                    <img
-                                        src="icons/logo-instagram.svg"
-                                        alt="Instagram logo"
-                                        width="40"
-                                        height="40"
-                                    />
-                                    </a>
-                                </div>
                             </div>
-                        </footer>
-                    `;
+                        </div>
+
+      <footer>
+        <div class="row-1">
+          <div class="col-1"><p>© Julian de Ligt 2024</p></div>
+          <div class="col-2">
+            <a
+              href="https://www.linkedin.com/in/julian-de-ligt-1586b9201?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=ios_app"
+              target="_blank"
+            >
+              <img
+                src="icons/logo-linkedin.svg"
+                alt="LinkedIn logo"
+                width="40"
+                height="40"
+              />
+            </a>
+            <a
+              href="https://www.instagram.com/juliandeligt_?igsh=bmNwbWdleWJnNWFp&utm_source=qr"
+              target="_blank"
+            >
+              <img
+                src="icons/logo-instagram.svg"
+                alt="Instagram logo"
+                width="40"
+                height="40"
+              />
+            </a>
+          </div>
+        </div>`;
                     container.innerHTML = projectTemplate;
                 } else {
                     console.error('Project not found:', projectName);
@@ -263,36 +286,5 @@ document.addEventListener('DOMContentLoaded', () => {
         }
     }
 
-    document.addEventListener('DOMContentLoaded', function() {
-        var form = document.getElementById('myForm');
-
-        form.addEventListener('submit', function(event) {
-            event.preventDefault(); // Voorkom standaard formulier verzenden
-
-            // Haal formulierdata op
-            var formData = new FormData(form);
-
-            // Verzenden van data naar een server
-            fetch('/path/to/your/server/endpoint', {
-                method: 'POST',
-                body: formData
-            })
-            .then(response => {
-                if (!response.ok) {
-                    throw new Error('Netwerkreactie was niet OK.');
-                }
-                return response.text();
-            })
-            .then(data => {
-                // Redirect naar de succespagina
-                window.location.href = '/succes.html';
-            })
-            .catch(error => {
-                console.error('Er was een probleem met de fetch-operatie:', error);
-            });
-        });
-    });
-    
     fetchAndDisplayProjectData();
-    
 });
